e150518d8a05c7308412301194fd039f10726312
Rev <53663385+reverset@users.noreply.github.com>
2025-04-25 18:19:00 -0700

    cleanup launcher

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -5,8 +5,16 @@ use std::{fs::File, io::Read, process::Command};
 const GAME_JAR_HASH: u64 = 10436466817374203798;
 const JAVAW_HASH: u64 = 11076107172332908793;
 
+const GAME_JAR_PATH: &str = "game.jar";
+
+#[cfg(target_os = "windows")]
+const JAVAW_PATH: &str = ".\\bin\\javaw.exe";
+
+#[cfg(target_os = "linux")]
+const JAVAW_PATH: &str = "./bin/javaw";
+
 fn check_game_jar() -> bool {
-    let mut file = File::open("game.jar").expect("Game.jar is missing.");
+    let mut file = File::open(GAME_JAR_PATH).expect("game.jar is missing.");
     let mut buffer = Vec::new();
     let _ = file.read_to_end(&mut buffer);    
 
@@ -15,7 +23,7 @@ fn check_game_jar() -> bool {
 }
 
 fn check_javaw() -> bool {
-    let mut file = File::open(".\\bin\\javaw.exe").expect(".\\bin\\javaw is missing.");
+    let mut file = File::open(JAVAW_PATH).expect("./bin/javaw is missing.");
     let mut buffer = Vec::new();
     let _ = file.read_to_end(&mut buffer);    
 
@@ -25,8 +33,8 @@ fn check_javaw() -> bool {
 
 fn main() {
     if check_game_jar() && check_javaw() {
-        Command::new(".\\bin\\javaw.exe")
-            .args(["-jar", "game.jar"])
+        Command::new(JAVAW_PATH)
+            .args(["-jar", GAME_JAR_PATH])
             .spawn().expect("failed to launch game.");
     }
 
